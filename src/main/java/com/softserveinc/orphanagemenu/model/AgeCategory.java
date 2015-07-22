7af8c66ad4de37f445def8c6d0be2313dfba28df
Serhii Dovhaniuk <Serhii.Dovhaniuk@gmail.com>
2015-07-22 21:09:53 +0300

    ageCategory, ProductWeight

--- src/main/java/com/softserveinc/orphanagemenu/model/AgeCategory.java
+++ src/main/java/com/softserveinc/orphanagemenu/model/AgeCategory.java
@@ -24,4 +24,68 @@ public class AgeCategory {
 
 	public AgeCategory() {
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((isActive == null) ? 0 : isActive.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AgeCategory other = (AgeCategory) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (isActive == null) {
+			if (other.isActive != null)
+				return false;
+		} else if (!isActive.equals(other.isActive))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
 }
---
+++ src/main/java/com/softserveinc/orphanagemenu/model/ProductWeight.java
@@ -0,0 +1,116 @@
+package com.softserveinc.orphanagemenu.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "product_weight")
+public class ProductWeight {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "standart_product_quantity")
+	private Double standartProductQuantity;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	@ManyToOne
+	@JoinColumn(name = "age_category_id")
+	private AgeCategory ageCategory;
+
+	public ProductWeight() {
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getStandartProductQuantity() {
+		return standartProductQuantity;
+	}
+
+	public void setStandartProductQuantity(Double standartProductQuantity) {
+		this.standartProductQuantity = standartProductQuantity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public AgeCategory getAgeCategory() {
+		return ageCategory;
+	}
+
+	public void setAgeCategory(AgeCategory ageCategory) {
+		this.ageCategory = ageCategory;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((ageCategory == null) ? 0 : ageCategory.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		result = prime
+				* result
+				+ ((standartProductQuantity == null) ? 0
+						: standartProductQuantity.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductWeight other = (ProductWeight) obj;
+		if (ageCategory == null) {
+			if (other.ageCategory != null)
+				return false;
+		} else if (!ageCategory.equals(other.ageCategory))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		if (standartProductQuantity == null) {
+			if (other.standartProductQuantity != null)
+				return false;
+		} else if (!standartProductQuantity
+				.equals(other.standartProductQuantity))
+			return false;
+		return true;
+	}
+	
+}
