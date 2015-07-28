af2636c49740576b98011a1728436c562483eb42
Andrii Vyhidnyi <wygidnyj@gmail.com>
2015-07-28 17:44:45 +0300

    add WarehouseForm and Validator

---
+++ src/main/java/com/softserveinc/orphanagemenu/validator/warehouse/WarehouseItemForm.java
@@ -0,0 +1,34 @@
+package com.softserveinc.orphanagemenu.validator.warehouse;
+
+public class WarehouseItemForm {
+	private Long id;
+	private String itemName;
+	private Float quantity;
+	private String dimension;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public Float getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Float quantity) {
+		this.quantity = quantity;
+	}
+	public String getDimension() {
+		return dimension;
+	}
+	public void setDimension(String dimension) {
+		this.dimension = dimension;
+	}
+	
+
+}
