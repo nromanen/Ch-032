b84fe7ec0de9774b980b3211c8e619ffb0652a70
Volodymyr Perepeliuk <vovan4uk86@gmail.com>
2015-07-28 20:14:58 +0300

    test form

--- src/main/java/com/softserveinc/orphanagemenu/service/ProductService.java
+++ src/main/java/com/softserveinc/orphanagemenu/service/ProductService.java
@@ -1,6 +1,9 @@
 ﻿package com.softserveinc.orphanagemenu.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,59 +11,61 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.softserveinc.orphanagemenu.dao.ProductDao;
+import com.softserveinc.orphanagemenu.forms.ProductForm;
 import com.softserveinc.orphanagemenu.model.AgeCategory;
 import com.softserveinc.orphanagemenu.model.Dimension;
 import com.softserveinc.orphanagemenu.model.Product;
 import com.softserveinc.orphanagemenu.model.ProductWeight;
-
+import com.softserveinc.orphanagemenu.model.UserAccount;
+import com.softserveinc.orphanagemenu.validator.user.UserAccountForm;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	@Qualifier("productDaoImpl")
 	private ProductDao productDAO;
-	
+
 	@Transactional
 	public void saveProduct(Product p) {
 		this.productDAO.saveProduct(p);
 	}
-	
+
 	@Transactional
-	public void updateProduct(Product product){
+	public void updateProduct(Product product) {
 		this.productDAO.updateProduct(product);
 	}
-	
+
 	public void updateProductWeight(ProductWeight productWeight) {
 		this.productDAO.updateProductWeight(productWeight);
 	}
-	
+
 	@Transactional
-	public ArrayList<Product> getAllProduct(String sort){
-    	return this.productDAO.getAllProduct(sort);
+	public ArrayList<Product> getAllProduct(String sort) {
+		return this.productDAO.getAllProduct(sort);
 	}
-	
+
 	@Transactional
 	public ArrayList<ProductWeight> getAllProductWeight() {
 		return this.productDAO.getAllProductWeight();
 	}
-	
+
 	@Transactional
-	public ArrayList<Dimension> getAllDimension(){
-    	return this.productDAO.getAllDimension();
+	public ArrayList<Dimension> getAllDimension() {
+		return this.productDAO.getAllDimension();
 	}
-	
+
 	public ArrayList<AgeCategory> getAllCategory() {
 		return this.productDAO.getAllCategory();
 	}
-	
+
 	@Transactional
-	public Product getProductById(Long id){
+	public Product getProductById(Long id) {
 		return this.productDAO.getProductById(id);
 	}
-	
+
 	@Transactional
-	public Dimension getDimensionById(Long id){
+	public Dimension getDimensionById(Long id) {
 		return this.productDAO.getDimensionById(id);
 	}
 
@@ -74,6 +79,25 @@ public class ProductService {
 
 	public void saveProductWeight(ProductWeight productWeight) {
 		this.productDAO.saveproductWeight(productWeight);
-		
+
+	}
+
+	public ProductForm getProductFormByProductId(Long id) {
+		ProductForm productForm = new ProductForm();
+		if (id == null) {
+			return productForm;
+		} else {
+			Product product = getProductById(id);
+			productForm.setId(product.getId().toString());
+			productForm.setName(product.getName());
+			productForm.setDimension(product.getDimension().getName());
+			Map<String, String> productWeights = new HashMap<>();
+			for (ProductWeight productWeight : product.getProductWeight()) {
+				productWeights.put("dimension_" + productWeight.getAgeCategory().getId(), productWeight
+						.getStandartProductQuantity().toString());
+			}
+			productForm.setWeight(productWeights);
+			return productForm;
+		}
 	}
 }
