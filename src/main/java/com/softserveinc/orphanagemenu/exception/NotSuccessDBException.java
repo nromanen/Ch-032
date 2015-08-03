d0e36b357882c05ee83dec856d39f22caaa9f2f9
Volodymyr Perepeliuk <vovan4uk86@gmail.com>
2015-08-03 22:05:33 +0300

    1

---
+++ src/main/java/com/softserveinc/orphanagemenu/exception/NotSuccessDBException.java
@@ -0,0 +1,10 @@
+package com.softserveinc.orphanagemenu.exception;
+
+public class NotSuccessDBException extends Exception {
+	private static final long serialVersionUID = -7418013762422574268L;
+
+	public NotSuccessDBException(String message){
+	     
+	        super(message);
+	 }
+}
