99cea25ad8167c7c7f30396ddfca314f50af1d25
unknown <dkhodtc@chclass06.training.local>
2015-08-05 17:26:52 +0300

    add dish

---
+++ src/main/java/com/softserveinc/orphanagemenu/model/Component.java
@@ -0,0 +1,112 @@
+package com.softserveinc.orphanagemenu.model;
+
+
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "component")
+public class Component {
+
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "meal_id")
+	private Dish dish;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	@OneToMany(mappedBy = "component",cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+	private Set<ComponentWeight> components;
+	
+	public Component() {}
+	
+	public Component(Dish dish, Product product){
+		this.dish = dish;
+		this.product = product;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Dish getDish() {
+		return dish;
+	}
+
+	public void setDish(Dish dish) {
+		this.dish = dish;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Set<ComponentWeight> getComponents() {
+		return components;
+	}
+
+	public void setComponents(Set<ComponentWeight> components) {
+		this.components = components;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Component other = (Component) obj;
+	
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		return true;
+	}
+	
+	
+
+}
