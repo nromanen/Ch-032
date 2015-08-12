70a1d54eff19d4e8dd2e4a7a0d4369d9adab42e4
Andrii Vyhidnyi <wygidnyj@gmail.com>
2015-08-12 16:58:53 +0300

    improved tests

---
+++ src/test/java/com/softserveinc/orphanagemenu/WarehouseServiceTest.java
@@ -0,0 +1,44 @@
+package com.softserveinc.orphanagemenu;
+
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.softserveinc.orphanagemenu.dao.WarehouseItemDaoImpl;
+import com.softserveinc.orphanagemenu.service.WarehouseServiceImpl;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/test-context.xml" })
+public class WarehouseServiceTest {
+
+	@InjectMocks
+	WarehouseServiceImpl warehouseService;
+
+	@Mock
+	private WarehouseItemDaoImpl mockDAO;
+
+	public void setUPTest() {
+
+		MockitoAnnotations.initMocks(this);
+		doReturn(15L).when(mockDAO).getCount();
+	}
+
+	@Test
+	public void testCount() {
+		setUPTest();
+
+		Long expected = 15L;
+
+		Long actual = warehouseService.getCount();
+
+		assertEquals(expected, actual);
+	}
+
+}
