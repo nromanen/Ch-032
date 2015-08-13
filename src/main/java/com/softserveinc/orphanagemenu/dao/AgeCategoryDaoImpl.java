1a0b8ba19a6f6ca0d9dd809bd227d9bf4e7c7581
Sviatoslav Fedechko <svjatoslavfedechko@gmail.com>
2015-08-13 12:03:37 +0300

    merge

--- src/main/java/com/softserveinc/orphanagemenu/dao/AgeCategoryDaoImpl.java
+++ src/main/java/com/softserveinc/orphanagemenu/dao/AgeCategoryDaoImpl.java
@@ -1,10 +1,13 @@
 package com.softserveinc.orphanagemenu.dao;
 
 import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.softserveinc.orphanagemenu.model.AgeCategory;
 
 @Repository("ageCategory")
