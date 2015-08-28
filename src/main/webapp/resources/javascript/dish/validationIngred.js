f4cd97098b94b785f3f667e12277d9e03cb51a1d
vkokhtc <vkokhtc@chclass07.training.local>
2015-08-28 17:09:26 +0300

    Merge Vlad2

---
+++ src/main/webapp/resources/javascript/dish/validationIngred.js
@@ -0,0 +1,69 @@
+$(function() {
+	
+	$("#updateDish").validate({
+		errorElement : 'div',
+		errorClass : 'frontEndError',
+		onfocusout : function(element) {
+			$(element).valid();
+		},
+		onkeyup : function(element) {
+			$(".error").remove();
+		},
+
+		rules : {
+		
+			
+		},
+		messages : {
+			
+		},
+		errorPlacement : function(error, element) {
+			error.insertAfter(element.closest('div'));
+		}
+
+	});
+	$('.wieghtClass').each(function() {
+		$(this).rules('add', {
+			required : true,
+			minlength : 1,
+			maxlength : 7,
+			pattern : /^([0-9])*([,|.]{0,1})[0-9]*$/,
+			messages : {
+				required : $('#productNormEmpty').html(),
+				minlength : $('#productNormTooShort').html(),
+				maxlength : $('#productNormTooLong').html(),
+				pattern : $('#weightIllegalCharacters').html()
+			}
+		});
+	});
+
+	$('#saveBtn').click(
+			function() {
+
+				if ($("#updateDish").valid() == true) {
+
+					$("#updateDish").submit();
+				}
+					
+
+
+			});
+
+	$('#cancelBtn').click(
+					function() {alert("123");
+						$
+								.confirm({
+									title : $('#submitChanges').html(),
+									text : $('#exitConfirmation').html(),									
+									confirmButton : $('#yes').html(),
+									cancelButton : $('#no').html(),
+									confirm : function() {
+										window.location.href = "editDish?=dishName=12";
+									},
+									cancel : function() {
+
+									}
+								});
+					});
+
+});
